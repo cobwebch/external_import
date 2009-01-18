6215b5506180fc6a1611ccc0c8eee08cbe6bc60f
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-01-18 16:38:09 +0000

    Released to TER as version 0.8.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/external_import/trunk@15783 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "external_import"
 #
-# Auto generated 04-08-2008 08:49
+# Auto generated 18-01-2009 17:37
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.7.5',
+	'version' => '0.8.0',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -38,7 +38,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:20:{s:9:"ChangeLog";s:4:"ecaf";s:10:"README.txt";s:4:"7e81";s:32:"class.tx_externalimport_ajax.php";s:4:"d46d";s:36:"class.tx_externalimport_autosync.php";s:4:"02e6";s:36:"class.tx_externalimport_importer.php";s:4:"43ec";s:21:"ext_conf_template.txt";s:4:"6848";s:12:"ext_icon.gif";s:4:"d913";s:17:"ext_localconf.php";s:4:"ab11";s:14:"ext_tables.php";s:4:"0fb5";s:13:"locallang.xml";s:4:"a0cf";s:33:"tx_externalimport_ajaxhandler.php";s:4:"d536";s:14:"doc/manual.sxw";s:4:"7783";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"f5bd";s:14:"mod1/index.php";s:4:"bd9d";s:18:"mod1/locallang.xml";s:4:"78cf";s:22:"mod1/locallang_mod.xml";s:4:"7a33";s:19:"mod1/moduleicon.gif";s:4:"b786";s:16:"res/prototype.js";s:4:"d3a5";s:30:"res/icons/refresh_animated.gif";s:4:"e1d4";}',
+	'_md5_values_when_last_written' => 'a:21:{s:9:"ChangeLog";s:4:"8774";s:10:"README.txt";s:4:"7e81";s:32:"class.tx_externalimport_ajax.php";s:4:"b4cc";s:36:"class.tx_externalimport_autosync.php";s:4:"ea0e";s:36:"class.tx_externalimport_importer.php";s:4:"3c91";s:21:"ext_conf_template.txt";s:4:"79f8";s:12:"ext_icon.gif";s:4:"d913";s:17:"ext_localconf.php";s:4:"ab11";s:14:"ext_tables.php";s:4:"0fb5";s:13:"locallang.xml";s:4:"a99a";s:33:"tx_externalimport_ajaxhandler.php";s:4:"3f16";s:14:"doc/manual.sxw";s:4:"baa4";s:13:"mod1/conf.php";s:4:"f5bd";s:14:"mod1/index.php";s:4:"550c";s:18:"mod1/locallang.xml";s:4:"e06a";s:22:"mod1/locallang_mod.xml";s:4:"7a33";s:19:"mod1/moduleicon.gif";s:4:"b786";s:16:"res/prototype.js";s:4:"d3a5";s:26:"res/icons/preview_data.gif";s:4:"526a";s:30:"res/icons/refresh_animated.gif";s:4:"e1d4";s:51:"samples/class.tx_externalimport_transformations.php";s:4:"bbc9";}',
 	'suggests' => array(
 	),
 );
