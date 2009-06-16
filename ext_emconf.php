1da9487bef97f6e43d7862d265669eb2ca65f86b
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-06-16 08:15:03 +0000

    Updated the manual for fixed values Updated the ChangeLog Added note in the manual about the tutorial Added suggestion for external_import_tut in ext_emconf.php
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/external_import/trunk@21462 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -36,6 +36,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'conflicts' => array(
 		),
 		'suggests' => array(
+			'external_import_tut' => ''
 		),
 	),
 	'_md5_values_when_last_written' => 'a:21:{s:9:"ChangeLog";s:4:"ae87";s:10:"README.txt";s:4:"7e81";s:32:"class.tx_externalimport_ajax.php";s:4:"9bde";s:36:"class.tx_externalimport_autosync.php";s:4:"12a0";s:36:"class.tx_externalimport_importer.php";s:4:"9ba2";s:21:"ext_conf_template.txt";s:4:"916e";s:12:"ext_icon.gif";s:4:"d913";s:17:"ext_localconf.php";s:4:"ab11";s:14:"ext_tables.php";s:4:"0fb5";s:13:"locallang.xml";s:4:"de2b";s:33:"tx_externalimport_ajaxhandler.php";s:4:"8a32";s:14:"doc/manual.sxw";s:4:"03fc";s:13:"mod1/conf.php";s:4:"f5bd";s:14:"mod1/index.php";s:4:"3426";s:18:"mod1/locallang.xml";s:4:"e06a";s:22:"mod1/locallang_mod.xml";s:4:"7a33";s:19:"mod1/moduleicon.gif";s:4:"b786";s:16:"res/prototype.js";s:4:"d3a5";s:26:"res/icons/preview_data.gif";s:4:"526a";s:30:"res/icons/refresh_animated.gif";s:4:"e1d4";s:51:"samples/class.tx_externalimport_transformations.php";s:4:"bbc9";}',
