2e3660104518f67ac36518c5d9619462e3f7681a
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-06-19 09:17:17 +0000

    New classes for scheduler and autoloader (TYPO3 4.3)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/external_import/trunk@21607 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ class.tx_externalimport_autosync_scheduler.php
@@ -0,0 +1,55 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2007-2009 Francois Suter (Cobweb) <typo3@cobweb.ch>
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+*
+* $Id: class.tx_externalimport_autosync.php 18238 2009-03-24 08:01:10Z francois $
+***************************************************************/
+
+require_once(t3lib_extMgm::extPath('external_import', 'class.tx_externalimport_importer.php'));
+
+/**
+ * This class executes Scheduler events for automatic synchronisations of external data
+ *
+ * @author		Francois Suter (Cobweb) <typo3@cobweb.ch>
+ * @package		TYPO3
+ * @subpackage	tx_externalimport
+ */
+class tx_externalimport_autosync_scheduler extends tx_scheduler_event {
+
+	/**
+	 * This method executes the task registered in the Scheduler event
+	 *
+	 * @return	void
+	 */
+	public function execute() {
+
+			// Get the crid for the event
+		$crid = $this->scheduler->getEventCrid($this->eventUid);
+
+			// Instantiate the import object and call appropriate method depending on command
+		$importer = t3lib_div::makeInstance('tx_externalimport_importer');
+		if ($this->commands['sync'] == 'all') {
+			$importer->synchronizeAllTables();
+		}
+	}
+}
+?>
---
+++ ext_autoload.php
@@ -0,0 +1,10 @@
+<?php
+/* 
+ * Register necessary class names with autoloader
+ *
+ * $Id: $
+ */
+return array(
+	'tx_externalimport_autosync_scheduler' => t3lib_extMgm::extPath('external_import', 'class.tx_externalimport_autosync_scheduler.php')
+);
+?>
