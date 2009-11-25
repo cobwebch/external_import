65c9126989bb52d3c4040958f7dfc5c5208bc4b6
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-11-25 09:23:06 +0000

    Released version 0.11.2
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/external_import/trunk@26940 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "external_import"
 #
-# Auto generated 23-11-2009 22:26
+# Auto generated 25-11-2009 09:46
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -30,7 +30,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.11.1',
+	'version' => '0.11.2',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 			'external_import_tut' => '',
 		),
 	),
-	'_md5_values_when_last_written' => 'a:30:{s:9:"ChangeLog";s:4:"83e3";s:10:"README.txt";s:4:"7e81";s:32:"class.tx_externalimport_ajax.php";s:4:"2525";s:36:"class.tx_externalimport_importer.php";s:4:"3bc3";s:16:"ext_autoload.php";s:4:"be7b";s:21:"ext_conf_template.txt";s:4:"88bd";s:12:"ext_icon.gif";s:4:"d913";s:17:"ext_localconf.php";s:4:"328e";s:14:"ext_tables.php";s:4:"fe73";s:13:"locallang.xml";s:4:"109b";s:33:"tx_externalimport_ajaxhandler.php";s:4:"2d3d";s:53:"autosync/class.tx_externalimport_autosync_factory.php";s:4:"1be3";s:59:"autosync/class.tx_externalimport_autosync_gabriel_event.php";s:4:"ac1d";s:79:"autosync/class.tx_externalimport_autosync_scheduler_additionalfieldprovider.php";s:4:"c12a";s:60:"autosync/class.tx_externalimport_autosync_scheduler_task.php";s:4:"07ce";s:53:"autosync/class.tx_externalimport_autosync_wrapper.php";s:4:"3cb0";s:61:"autosync/class.tx_externalimport_autosync_wrapper_gabriel.php";s:4:"67ca";s:63:"autosync/class.tx_externalimport_autosync_wrapper_scheduler.php";s:4:"f180";s:14:"doc/manual.sxw";s:4:"7347";s:13:"mod1/conf.php";s:4:"f5bd";s:14:"mod1/index.php";s:4:"8aef";s:18:"mod1/locallang.xml";s:4:"904c";s:37:"mod1/locallang_csh_externalimport.xml";s:4:"06dd";s:22:"mod1/locallang_mod.xml";s:4:"7a33";s:19:"mod1/moduleicon.gif";s:4:"b786";s:16:"res/prototype.js";s:4:"d3a5";s:24:"res/tx_externalimport.js";s:4:"009a";s:26:"res/icons/preview_data.gif";s:4:"526a";s:30:"res/icons/refresh_animated.gif";s:4:"e1d4";s:51:"samples/class.tx_externalimport_transformations.php";s:4:"bbc9";}',
+	'_md5_values_when_last_written' => 'a:30:{s:9:"ChangeLog";s:4:"065d";s:10:"README.txt";s:4:"7e81";s:32:"class.tx_externalimport_ajax.php";s:4:"2525";s:36:"class.tx_externalimport_importer.php";s:4:"98da";s:16:"ext_autoload.php";s:4:"be7b";s:21:"ext_conf_template.txt";s:4:"88bd";s:12:"ext_icon.gif";s:4:"d913";s:17:"ext_localconf.php";s:4:"328e";s:14:"ext_tables.php";s:4:"fe73";s:13:"locallang.xml";s:4:"109b";s:33:"tx_externalimport_ajaxhandler.php";s:4:"2d3d";s:53:"autosync/class.tx_externalimport_autosync_factory.php";s:4:"1be3";s:59:"autosync/class.tx_externalimport_autosync_gabriel_event.php";s:4:"ac1d";s:79:"autosync/class.tx_externalimport_autosync_scheduler_additionalfieldprovider.php";s:4:"c12a";s:60:"autosync/class.tx_externalimport_autosync_scheduler_task.php";s:4:"07ce";s:53:"autosync/class.tx_externalimport_autosync_wrapper.php";s:4:"3cb0";s:61:"autosync/class.tx_externalimport_autosync_wrapper_gabriel.php";s:4:"67ca";s:63:"autosync/class.tx_externalimport_autosync_wrapper_scheduler.php";s:4:"f180";s:14:"doc/manual.sxw";s:4:"5e9a";s:13:"mod1/conf.php";s:4:"f5bd";s:14:"mod1/index.php";s:4:"8aef";s:18:"mod1/locallang.xml";s:4:"904c";s:37:"mod1/locallang_csh_externalimport.xml";s:4:"06dd";s:22:"mod1/locallang_mod.xml";s:4:"7a33";s:19:"mod1/moduleicon.gif";s:4:"b786";s:16:"res/prototype.js";s:4:"d3a5";s:24:"res/tx_externalimport.js";s:4:"009a";s:26:"res/icons/preview_data.gif";s:4:"526a";s:30:"res/icons/refresh_animated.gif";s:4:"e1d4";s:51:"samples/class.tx_externalimport_transformations.php";s:4:"bbc9";}',
 	'suggests' => array(
 	),
 );
