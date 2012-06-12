cd1bafda28e3136a80d94719f538d008885d6625
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-06-12 11:56:54 +0000

    Removed old module registration code, references #5106
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/external_import/trunk@63346 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -1,12 +1,6 @@
 <?php
 if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 
-if (TYPO3_MODE == 'BE')	{
-//	t3lib_extMgm::addModule('user', 'txexternalimportM1', '', t3lib_extMgm::extPath($_EXTKEY) . 'mod1/');
-
-		// Add context sensitive help (csh) to the backend module
-//	t3lib_extMgm::addLLrefForTCAdescr('_MOD_user_txexternalimportM1', 'EXT:' . $_EXTKEY . '/mod1/locallang_csh_externalimport.xml');
-}
 	// Avoid loading the module when in the frontend or the Install Tool
 if (TYPO3_MODE == 'BE' && !(TYPO3_REQUESTTYPE & TYPO3_REQUESTTYPE_INSTALL)) {
 		// Register the backend module
