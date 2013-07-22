0a789f5e0f89429e6da375ff0f93dab5c73c6f1c
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-07-22 19:32:37 +0000

    Updated compatibility information, references #50294
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/external_import/trunk@78144 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -35,7 +35,7 @@ $EM_CONF[$_EXTKEY] = array(
 			'svconnector' => '2.0.0-0.0.0',
 			'extbase' => '1.3.0-0.0.0',
 			'fluid' => '1.3.0-0.0.0',
-			'typo3' => '4.5.0-4.7.99',
+			'typo3' => '4.5.0-6.1.99',
 		),
 		'conflicts' => array(
 		),
