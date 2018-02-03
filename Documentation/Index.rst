4432a742b67bc93ffbe1731a9c3f7308378c4944
Francois Suter <fsuter@cobweb.ch>
2018-02-03 20:59:02 +0100

    [TASK] Release version 4.0.1

--- Documentation/Index.rst
+++ Documentation/Index.rst
@@ -22,7 +22,7 @@ External Import
 		  data import, fetch data, services
 
 	:Copyright:
-		2007-2017
+		2007-2018
 
 	:Author:
 		  François Suter (Cobweb)
--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array (
   'createDirs' => '',
   'clearCacheOnLoad' => 0,
   'author_company' => '',
-  'version' => '4.0.0',
+  'version' => '4.0.1',
   'constraints' =>
   array (
     'depends' =>
