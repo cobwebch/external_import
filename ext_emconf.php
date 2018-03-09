6b8b059bed30179a3b59f444b80a0c1bb69ac629
Francois Suter <fsuter@cobweb.ch>
2018-03-09 22:26:08 +0100

    [TASK] Release version 4.0.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array (
   'createDirs' => '',
   'clearCacheOnLoad' => 0,
   'author_company' => '',
-  'version' => '4.0.1',
+  'version' => '4.0.2',
   'constraints' =>
   array (
     'depends' =>
