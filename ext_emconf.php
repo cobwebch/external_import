e583c92af991021edea76b530912141ce73cd1c2
Francois Suter <francois.suter@ideative.ch>
2020-04-14 22:00:43 +0200

    [TASK] Release version 4.2.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = [
         'createDirs' => '',
         'clearCacheOnLoad' => 0,
         'author_company' => '',
-        'version' => '4.2.1',
+        'version' => '4.2.2',
         'constraints' =>
                 [
                         'depends' =>
