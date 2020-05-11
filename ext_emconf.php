7ff68629be5e8ffb1bb1bb73c2fbcae9e1616a64
Francois Suter <francois.suter@ideative.ch>
2020-05-11 18:15:23 +0200

    [BUGFIX] Fix wrong compat information :-(

--- ext_emconf.php
+++ ext_emconf.php
@@ -27,7 +27,7 @@ $EM_CONF[$_EXTKEY] = [
                         'depends' =>
                                 [
                                         'svconnector' => '3.4.0-0.0.0',
-                                        'typo3' => '8.5.0-9.99.99',
+                                        'typo3' => '8.5.0-10.4.99',
                                 ],
                         'conflicts' =>
                                 [
