1f42e6085b6afb1b10cdaa15ed359504e6109484
François Suter <fsuter@users.noreply.github.com>
2021-04-06 12:51:46 +0200

    Prepare for TYPO3 11, drop support for TYPO3 9

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = [
                         'depends' =>
                                 [
                                         'svconnector' => '3.4.0-0.0.0',
-                                        'typo3' => '9.5.0-10.4.99',
+                                        'typo3' => '10.4.99-11.99.99',
                                         'scheduler' => '',
                                 ],
                         'conflicts' =>
