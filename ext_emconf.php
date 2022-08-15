899977b0febd5c42372fdcf0214d87caa140805a
Francois Suter <sutfra@gmail.com>
2022-08-15 08:53:29 +0200

    [TASK] Release version 6.0.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
     'author_company' => '',
-    'version' => '6.0.3',
+    'version' => '6.0.4',
     'constraints' =>
         [
             'depends' =>
