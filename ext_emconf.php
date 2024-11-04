ac9f04d78e2f77debd2a8f442780048c43490baa
Francois Suter <sutfra@gmail.com>
2024-11-04 21:03:14 +0100

    [TASK] Release version 7.2.7

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
     'author_company' => '',
-    'version' => '7.2.6',
+    'version' => '7.2.7',
     'constraints' =>
         [
             'depends' =>
