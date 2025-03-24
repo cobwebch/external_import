beae96e8313cb10e9de40f1db507e237f8ee8822
Francois Suter <sutfra@gmail.com>
2025-03-24 11:28:22 +0100

    [TASK] Release version 8.0.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'typo3@ideative.ch',
     'state' => 'stable',
     'author_company' => '',
-    'version' => '8.0.3',
+    'version' => '8.0.4',
     'constraints' =>
         [
             'depends' =>
